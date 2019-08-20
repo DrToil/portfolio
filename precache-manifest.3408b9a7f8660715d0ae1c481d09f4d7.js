f14af5e3c89ece06b471357109b0b30fbbfcf11b
Dr Toil <drtoil@Drs-MacBook-Pro.local>
2019-08-20 12:27:00 +0100

    Updates

---
+++ precache-manifest.3408b9a7f8660715d0ae1c481d09f4d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2dae271f206c9ed558dcff5f08d06a6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e1f5f27b35eae5445f1e",
+    "url": "/portfolio/static/css/main.03c74ac6.chunk.css"
+  },
+  {
+    "revision": "f80f0acbd2d0ce47e57c",
+    "url": "/portfolio/static/js/2.917ae229.chunk.js"
+  },
+  {
+    "revision": "e1f5f27b35eae5445f1e",
+    "url": "/portfolio/static/js/main.a9b45451.chunk.js"
+  },
+  {
+    "revision": "e8aab94e553ef1acd8d3",
+    "url": "/portfolio/static/js/runtime~main.e9a507a1.js"
+  },
+  {
+    "revision": "8990aa4273f67dba5cc15a34d46f6a03",
+    "url": "/portfolio/static/media/bck-header-img.8990aa42.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c247a0908d07b56cd108fc1a110adada.js"
+  "/portfolio/precache-manifest.3408b9a7f8660715d0ae1c481d09f4d7.js"
 );
 
 self.addEventListener('message', (event) => {
