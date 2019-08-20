e922496e0a535626487ddf6c948543dfb4543b28
Dr Toil <drtoil@Drs-MacBook-Pro.local>
2019-08-20 11:11:07 +0100

    starting clearn app for my portfolio.

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ function App() {
         <h1>Ahmed Ali</h1>
 
           <h4>Welcome to my online space where I show off my skills.</h4>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
