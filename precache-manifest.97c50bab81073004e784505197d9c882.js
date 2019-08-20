ae565fd381c869be7f7c4f82ce378c6ca4faddc0
Dr Toil <drtoil@Drs-MacBook-Pro.local>
2019-08-20 10:11:31 +0100

    Updates

---
+++ precache-manifest.97c50bab81073004e784505197d9c882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90be10cc5dde262c828bd6855426341",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "a9f616fef4f056a4f52e",
+    "url": "/portofolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f80f0acbd2d0ce47e57c",
+    "url": "/portofolio/static/js/2.917ae229.chunk.js"
+  },
+  {
+    "revision": "a9f616fef4f056a4f52e",
+    "url": "/portofolio/static/js/main.dbf664d7.chunk.js"
+  },
+  {
+    "revision": "f25d98defe2ede116f75",
+    "url": "/portofolio/static/js/runtime~main.cdd90c65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portofolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
