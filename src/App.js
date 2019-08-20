047139730beedfde7fcf49b7ddabd8935b39fc3f
Dr Toil <drtoil@Drs-MacBook-Pro.local>
2019-08-20 12:26:37 +0100

    added image and navigation bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
+import bckimg from './img/bck-header-img.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Ahmed Ali</h1>
+        <div className='headerNav'>
+            <div className='navItems'>Home</div>
+            <div className='navItems'>About</div>
+            <div className='navItems'>Contact</div>
+        </div>
+        <div className='headerImg'>
+          <div><img src={bckimg} alt='backgorund header' />
+            <h1>Ahmed Ali</h1>
+          </div>
+          
 
-          <h4>Welcome to my online space where I show off my skills.</h4>
-      
-      </header>
+        </div>
+
     </div>
   );
 }
