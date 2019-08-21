62faf8c0c3747cbd7798eb681e281cca128aa6fa
Dr Toil <drtoil@Drs-MacBook-Pro.local>
2019-08-21 14:12:30 +0100

    made changes to the nav bar. used grid.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import './App.css';
-import bckimg from './img/bck-header-img.jpg';
+import React from "react";
+import "./App.css";
+import bckimg from "./img/bck-header-img.jpg";
 
 function App() {
   return (
     <div className="App">
-        <div className='headerNav'>
-            <div className='navItems'>Home</div>
-            <div className='navItems'>About</div>
-            <div className='navItems'>Contact</div>
+      <header>
+        <div className="nav-container">
+          <nav class="nav-checkbox">
+            <a href="#" className="logo">
+              <h1>(DT)/></h1>
+            </a>
+            <input id="tab-nav" type="checkbox" class="tab-nav" name="tabs" />
+            <label for="tab-nav" class="tab-nav-label">
+              Menu
+            </label>
+            <ul class="tab-content">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className='headerImg'>
-          <img src={bckimg} alt='backgorund header' />
-            <h1>Dr Toil</h1>
-        </div>
-
+      </header>
+      <div className="headerImg">
+        <img src={bckimg} alt="backgorund header" />
+        <h1>Dr Toil</h1>
+      </div>
     </div>
   );
 }
