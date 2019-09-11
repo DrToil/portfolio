8bd03098dea04781e0fbf95bb6a7b868cc02dad6
DrToil <you@example.com>
2019-09-11 19:23:08 +0100

    added skill & about sections

--- src/App.js
+++ src/App.js
@@ -38,6 +38,48 @@ function App() {
           <h1>Dr Toil</h1>
         </div>
       </section>
+      <section id="aboutSection">
+        <h2>About Me</h2>
+        <p>
+          Technology is amazing it enables us to do more things, faster and
+          accurate.
+        </p>
+        <p>
+          I wanted to pursue a career in computing since I was 12. I used to buy
+          magazines that offered free tutorial usually in form of DVD then use
+          to try on what I have learned. I continued to this path until 2010
+          when I got my Higher Diploma in Computer Science.
+        </p>
+        <p>
+          Then I started my own business starting the opening of an Internet
+          Cafe where I used to provide IT services such as repairing computers,
+          selling them after I bought then in Bulks. I also provided computers
+          repair service to other internet cafe's.
+        </p>
+        <p>
+          After many years of trying different types of business than failing I
+          finally decided to stick to my childhood dream which I was good and
+          still good at anyway.
+        </p>
+        <p>
+          I am a quick learner and I am open for training. I can start as a
+          volunteer if you are looking for someone to hire who will make a
+          sizeable contribution to you are organization look no further.
+        </p>
+        <p>Thank you for reading, I will look forward to hearing from you.</p>
+      </section>
+      <skills id='skillSection'>
+          <h2> skills </h2>
+
+        <div className='flex-item'>HTML</div>
+        <div className='flex-item'>CSS</div>
+        <div className='flex-item'>SASS</div>
+        <div className='flex-item'>JavaScript</div>
+        <div className='flex-item'>React</div>
+        <div className='flex-item'>MongoDB</div>
+        <div className='flex-item'>GitHub</div>
+
+      </skills>
     </div>
   );
 }
