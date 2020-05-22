793d6ef0ebdfb18e2e81898c549931cf03f343ad
DrToil <axmed13@hotmail.com>
2020-05-22 17:24:25 +0100

    created component folder in which I added some pages

---
+++ src/components/Skills.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <skills id='skillSection'>
+      <h2> SKILLS </h2>
+
+      <div className='flex-item'>HTML</div>
+      <div className='flex-item'>CSS</div>
+      <div className='flex-item'>SASS</div>
+      <div className='flex-item'>JavaScript</div>
+      <div className='flex-item'>React</div>
+      <div className='flex-item'>MongoDB</div>
+      <div className='flex-item'>GitHub</div>
+    </skills>
+  );
+}
+export default Skills;
